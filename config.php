5725268ef821f2fefa6f06cb05d367169843a7e3
lopingbest <setiadi.galih65@gmail.com>
2021-05-24 18:39:34 +0700

    api

---
+++ config.php
@@ -0,0 +1,9 @@
+ <?php
+ define('HOST','localhost');
+ define('USER','root');
+ define('PASS','root');
+ define('DB','TumbuhinV2');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+ 
+
