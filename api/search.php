f1bbf9db98677e3360578ab422077ef791aa960e
Galih Setiadi <46417416+lopingbest@users.noreply.github.com>
2021-05-28 15:42:57 +0700

    Update search.php

--- api/search.php
+++ api/search.php
@@ -2,11 +2,11 @@
 require_once('config.php');
 if($_SERVER['REQUEST_METHOD']=='POST') {
   $search = $_POST['search'];
-  $sql = "SELECT * FROM tb_datatumbuhan where nama or manfaat LIKE '%$search%' ORDER BY nama ASC";
+  $sql = "SELECT * FROM tb_datatumbuhan where nama LIKE '%$search%' ORDER BY nama ASC";
   $res = mysqli_query($con,$sql);
   $result = array();
   while($row = mysqli_fetch_array($res)){
-    array_push($result, array('id'=>$row[0], 'nama'=>$row[1], 'nama_latin'=>$row[2], 'manfaat'=>$row[3], 'komposis>
+    array_push($result, array('id'=>$row[0], 'nama'=>$row[1], 'nama_latin'=>$row[2], 'manfaat'=>$row[3], 'komposisi'=>$row[4]>
   }
   echo json_encode(array("value"=>1,"result"=>$result));
   mysqli_close($con);
