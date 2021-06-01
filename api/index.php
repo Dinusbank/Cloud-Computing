4a2a8604d3076ac53d62d93e8234b5b6e1033ecf
Galih Setiadi <46417416+lopingbest@users.noreply.github.com>
2021-06-01 13:44:27 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,8 +5,9 @@ if($_SERVER['REQUEST_METHOD']=='GET') {
   $res = mysqli_query($con,$sql);
   $result = array();
   while($row = mysqli_fetch_array($res)){
-    array_push($result, array('id'=>$row[0], 'nama'=>$row[1],'nama_latin'=>$row[2], 'manfaat'=>$row[3], 'komposisi'=>$row[4]>
+    array_push($result, array('id'=>$row[0], 'nama'=>$row[1],'nama_latin'=>$row[2], 'manfaat'=>$row[3], 'kandungan'=>$row[4], 'gambar'=>$row[5]));
   }
   echo json_encode(array("value"=>1,"result"=>$result));        
   mysqli_close($con);
 }
+?>
