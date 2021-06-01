85f11e38320fd98c3344a9c05560ee8b916f81f2
Galih Setiadi <46417416+lopingbest@users.noreply.github.com>
2021-06-01 13:43:39 +0700

    Delete urgen2 directory

--- urgen2/config.php
+++
@@ -1,8 +0,0 @@
-<?php
- define('HOST','localhost');
- define('USER','root');
- define('PASS','');
- define('DB','tumbuhr');
-
- $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
- ?>
